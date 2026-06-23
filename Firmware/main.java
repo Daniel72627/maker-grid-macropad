6cf24a1023abb449f88199d606dfdf41e65ea8b2
Daniel <danieldenava04@gmail.com>
2026-06-23 15:42:02 -0700

    v0.0

--- Firmware/main.java
+++ Firmware/main.java
@@ -3,5 +3,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("This is the main class of the firmware.");
+        System.out.println("This is my first project beware.");
+
     }
 }
