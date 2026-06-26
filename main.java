8711c8905a0855618c932b1c3acaacf827afe672
Daniel <danieldenava04@gmail.com>
2026-06-26 00:30:00 -0700

    v0.5

--- main.java
+++ main.java
@@ -2,6 +2,18 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("This is a sample Java program.");
+        System.out.println("This is a new project");
+        System.out.println("This is a new project");
+        System.out.println("This is a new project");
+        System.out.println("This is a new project");
+
+        int num = 1;
+        double robot_name = 2;
+        String name = "John Pork";
+        System.out.println("This is a new project");
+        System.out.println("This is a new project");
+        System.out.println("This is a new project");
+
         
     }
 }
